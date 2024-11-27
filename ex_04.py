dd2c749a2a879c142b2db505a8a4ad1a2f50da07
MondoukpeStella <stella.aguemon@epitech.eu>
2024-11-27 11:50:45 +0100

    [ADD]: ex_04 added

--- ex_04.py
+++ ex_04.py
@@ -1,2 +1,6 @@
 def find_missing_number(liste):
-    pass
+    n = len(liste) + 1  
+    sum_with_missing = (min(liste) + max(liste)) * n // 2 
+    actual_sum = sum(liste)
+
+    return sum_with_missing - actual_sum
